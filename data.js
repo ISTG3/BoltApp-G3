a61cc5d8d834d5aa55b6207e58b923c847f71e69
Yvon <yvonmanzi@gmail.com>
2021-12-02 20:44:05 +0300

    d3 stuff

---
+++ data.js
@@ -0,0 +1,7 @@
+const { now } = require("d3-timer");
+
+var data_entry = {
+    'when': Date(now),
+    'race': 100,
+
+}
--- graph/chart.js
+++ graph/chart.js
@@ -1,49 +1,107 @@
-// Sample dataset. In a real application, you will probably get this data from another source such as AJAX.
-var dataset = [5, 10, 15, 20, 25]
+import * as d3 from 'd3'
+var data = [{
+        "letter": "a",
+        "frequency": 15
+    },
+    {
+        "letter": "b",
+        "frequency": 20
+    },
+    {
+        "letter": "c",
+        "frequency": 25
+    },
+    {
+        "letter": "d",
+        "frequency": 30
+    },
 
-// Sizing variables for our chart. These are saved as variables as they will be used in calculations.
-var chartWidth = 300
-var chartHeight = 100
-var padding = 5
+];
+// var data = [1, 2, 3, 34, 56, 7, 1, 2, 2];
 
-// We want our our bars to take up the full height of the chart, so, we will apply a scaling factor to the height of every bar.
-var heightScalingFactor = chartHeight / getMax(dataset)
+function LineChart(data, {
+    x = ([x]) => x, // given d in data, returns the (temporal) x-value
+    y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
+    defined, // for gaps in data
+    curve = d3.curveLinear, // method of interpolation between points
+    marginTop = 20, // top margin, in pixels
+    marginRight = 30, // right margin, in pixels
+    marginBottom = 30, // bottom margin, in pixels
+    marginLeft = 40, // left margin, in pixels
+    width = 640, // outer width, in pixels
+    height = 400, // outer height, in pixels
+    xType = d3.scaleUtc, // the x-scale type
+    xDomain, // [xmin, xmax]
+    xRange = [marginLeft, width - marginRight], // [left, right]
+    yType = d3.scaleLinear, // the y-scale type
+    yDomain, // [ymin, ymax]
+    yRange = [height - marginBottom, marginTop], // [bottom, top]
+    yFormat, // a format specifier string for the y-axis
+    yLabel, // a label for the y-axis
+    color = "currentColor", // stroke color of line
+    strokeLinecap = "round", // stroke line cap of the line
+    strokeLinejoin = "round", // stroke line join of the line
+    strokeWidth = 1.5, // stroke width of line, in pixels
+    strokeOpacity = 1, // stroke opacity of line
+} = {}) {
+    // Compute values.
+    const X = d3.map(data, x);
+    const Y = d3.map(data, y);
+    const I = d3.range(X.length);
+    if (defined === undefined) defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
+    const D = d3.map(data, defined);
 
-// Here we are creating the SVG that will be our chart.
-var svg = d3
-    .select('#my-chart') // I'm starting off by selecting the container.
-    .append('svg') // Appending an SVG element to that container.
-    .attr('width', chartWidth) // Setting the width of the SVG.
-    .attr('height', chartHeight) // And setting the height of the SVG.
+    // Compute default domains.
+    if (xDomain === undefined) xDomain = d3.extent(X);
+    if (yDomain === undefined) yDomain = [0, d3.max(Y)];
 
-// The next step is to create the rectangles that will make up the bars in our bar chart.
-svg
-    .selectAll('rect') // I'm selecting all of the rectangles in the SVG (note that at this point, there actually aren't any, but we'll be creating them in a couple of steps).
-    .data(dataset) // Then I'm mapping the dataset to those rectangles.
-    .enter() // This step is important in that it allows us to dynamically create the rectangle elements that we selected previously.
-    .append('rect') // For each element in the dataset, append a new rectangle.
-    .attr('x', function(value, index) { // Set the X position of the rectangle by taking the index of the current item we are creating, multiplying it by the calculated width of each bar, and adding a padding value so we can see some space between bars.
-        return (index * (chartWidth / dataset.length)) + padding
-    })
-    .attr('y', function(value, index) { // Set the rectangle by subtracting the scaled height from the height of the chart (this has to be done becuase SVG coordinates start with 0,0 at their top left corner).
-        return chartHeight - (value * heightScalingFactor)
-    })
-    .attr('width', (chartWidth / dataset.length) - padding) // The width is dynamically calculated to have an even distribution of bars that take up the entire width of the chart.
-    .attr('height', function(value, index) { // The height is simply the value of the item in the dataset multiplied by the height scaling factor.
-        return value * heightScalingFactor
-    })
-    .attr('fill', 'pink') // Sets the color of the bars.
+    // Construct scales and axes.
+    const xScale = xType(xDomain, xRange);
+    const yScale = yType(yDomain, yRange);
+    const xAxis = d3.axisBottom(xScale).ticks(width / 80).tickSizeOuter(0);
+    const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
 
-/**
- *  Gets the maximum value in a collection of numbers.
- */
-function getMax(collection) {
-    var max = 0
+    // Construct a line generator.
+    const line = d3.line()
+        .defined(i => D[i])
+        .curve(curve)
+        .x(i => xScale(X[i]))
+        .y(i => yScale(Y[i]));
 
-    collection.forEach(function(element) {
-        max = element > max ? element : max
-    })
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
-    return max
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(yAxis)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick line").clone()
+            .attr("x2", width - marginLeft - marginRight)
+            .attr("stroke-opacity", 0.1))
+        .call(g => g.append("text")
+            .attr("x", -marginLeft)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text(yLabel));
+
+    svg.append("path")
+        .attr("fill", "none")
+        .attr("stroke", color)
+        .attr("stroke-width", strokeWidth)
+        .attr("stroke-linecap", strokeLinecap)
+        .attr("stroke-linejoin", strokeLinejoin)
+        .attr("stroke-opacity", strokeOpacity)
+        .attr("d", line(I));
+
+    return svg.node();
 }
-// max(collection)
+graph = LineChart(data)
+document.getElementById('my-chart').appendChild(graph)
