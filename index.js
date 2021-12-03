004c6179f4ea6e9929043d6f5bdbe1c99181e401
Yvon <yvonmanzi@gmail.com>
2021-12-03 07:31:27 +0300

    progress on sending and getting data

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express, { response } from 'express';
 import hbs from 'hbs';
+import fs from 'fs';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
+const jsonParser = bodyParser.json();
+const fileName = 'runs.json';
+
+
+
+// Load data from file
+let rawData = fs.readFileSync(fileName);
+let data = JSON.parse(rawData);
+console.log("data:", data)
 
 app.set('views', 'views');
 app.set('view engine', 'hbs');
@@ -14,5 +25,18 @@ hbs.registerPartials("views/partials"), err => {
     }
 }
 app.get('/', (req, res) => res.render('home'))
+
+app.get('/runs', (req, res) => {
+    data.sort((a, b) => (a.runDate > b.runDate) ? 1 : -1);
+    response.send(data);
+
+});
+app.post('/runs', jsonParser, (request, response) => {
+    data.push(request.body);
+    fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
+    response.end();
+});
+
+
 
 app.listen(port, () => console.log("I am listening on ", port))
